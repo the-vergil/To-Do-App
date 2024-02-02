c3e554318c1dfe4895cf5bebb569402bcf55b2c4
the-vergil <chetmani1033@gmail.com>
2024-02-02 11:37:56 +0530

    error handling

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.send.FRONTEND_URL],
+    // origin: [process.send.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
